39e203f3458b589845b1281ecab0dba1e89f670b
James Allen <jamesallen0108@gmail.com>
2012-02-04 12:31:41 -0500

    Set up database for use with CLSI

--- clsi/recipes/default.rb
+++ clsi/recipes/default.rb
@@ -3,8 +3,25 @@
 #
 # Copyright 2012, ScribTeX
 
-node.default[:clsi][:install_directory] = "/var/www/clsi"
-node.default[:clsi][:user]              = "www-data"
+node.default[:clsi][:install_directory]   = "/var/www/clsi"
+node.default[:clsi][:user]                = "www-data"
+node.default[:clsi][:database][:name]     = "clsi"
+node.default[:clsi][:database][:user]     = "clsi"
+node.default[:clsi][:database][:password] = ""
+
+mysql_connection = ({:host => "localhost", :username => 'root', :password => node['mysql']['server_root_password']})
+
+# Set up the database
+mysql_database node[:clsi][:database][:name] do
+  connection mysql_connection
+  action     :create
+end
+
+mysql_database_user node[:clsi][:database][:user] do
+  connection mysql_connection
+  password   node[:clsi][:database][:password]
+  action     :grant
+end
 
 package "git-core"
 
@@ -19,8 +36,33 @@ directory node[:clsi][:install_directory] do
   recursive true
 end
 
-deploy "/var/www/clsi" do
+deploy node[:clsi][:install_directory] do
   repo     "git://github.com/scribtex/clsi.git"
   revision "master"
   user     node[:clsi][:user]
+
+  environment ({
+    "RAILS_ENV" => "production"
+  })
+  migrate           true
+  migration_command "rake db:migrate"
+  symlink_before_migrate ({
+    "config/database.yml" => "config/database.yml",
+    "config/config.yml"   => "config/config.yml",
+    "config/mailer.yml"   => "config/mailer.yml"
+  })
+  before_migrate do
+    directory "#{node[:clsi][:install_directory]}/shared/config" do
+      owner  node[:clsi][:user]
+    end
+
+    template "#{node[:clsi][:install_directory]}/shared/config/database.yml" do
+      source "config/database.yml"
+      owner  node[:clsi][:user]
+    end
+    template "#{node[:clsi][:install_directory]}/shared/config/config.yml" do
+      source "config/config.yml"
+      owner  node[:clsi][:user]
+    end
+  end
 end
